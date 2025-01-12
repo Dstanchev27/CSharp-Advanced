2c947744ee3885d801160ed248df17e0d1066943
Dstanchev27 <d.stanchev2007@gmail.com>
2025-01-12 18:42:48 +0200

    init

--- MultidimensionalArrays-Lab/5SquareWithMaximumSum/obj/Debug/net8.0/5SquareWithMaximumSum.AssemblyInfo.cs
+++ MultidimensionalArrays-Lab/5SquareWithMaximumSum/obj/Debug/net8.0/5SquareWithMaximumSum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("5SquareWithMaximumSum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9866ab3a798e9494a430319060db32d0d5dd5238")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf4970a50a444353a211d18aa532d5080b02b3bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("5SquareWithMaximumSum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("5SquareWithMaximumSum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
