e4d7c678b831537199e8263bd9cdfbede516da67
Dstanchev27 <d.stanchev2007@gmail.com>
2025-02-06 22:04:53 +0200

    init

---
+++ DefiningClasses2/PokemonTrainer/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace PokemonTrainer
+{
+    public class Program
+    {
+        public class StartUp
+        {
+            public static void Main()
+            {
+                var trainers = new List<Trainer>();
+
+                string line;
+                while ((line = Console.ReadLine()) != "Tournament")
+                {
+                    var tokens = line.Split();
+                    string trainerName = tokens[0];
+                    string pokemonName = tokens[1];
+                    string pokemonElement = tokens[2];
+                    int pokemonHealth = int.Parse(tokens[3]);
+
+                    var trainer = trainers.FirstOrDefault(t => t.Name == trainerName);
+                    if (trainer == null)
+                    {
+                        trainer = new Trainer(trainerName);
+                        trainers.Add(trainer);
+                    }
+
+                    var pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+                    trainer.Pokemons.Add(pokemon);
+                }
+
+                while ((line = Console.ReadLine()) != "End")
+                {
+                    string element = line;
+
+                    foreach (var trainer in trainers)
+                    {
+                        var hasElement = trainer.Pokemons.Any(p => p.Element == element);
+
+                        if (hasElement)
+                        {
+                            trainer.EarnBadge();
+                        }
+                        else
+                        {
+                            trainer.LoseHealth(element);
+                        }
+                    }
+                }
+
+                foreach (var trainer in trainers.OrderByDescending(t => t.Badges).ThenBy(t => trainers.IndexOf(t)))
+                {
+                    Console.WriteLine($"{trainer.Name} {trainer.Badges} {trainer.Pokemons.Count}");
+                }
+            }
+        }
+    }
+}
---
+++ DefiningClasses2/PokemonTrainer/Trainer.cs
@@ -0,0 +1,41 @@
+﻿namespace PokemonTrainer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Trainer
+    {
+        public string Name { get; set; }
+        public int Badges { get; set; } = 0;
+        public List<Pokemon> Pokemons { get; set; }
+
+        public Trainer(string name)
+        {
+            Name = name;
+            Pokemons = new List<Pokemon>();
+        }
+
+        public void EarnBadge()
+        {
+            Badges++;
+        }
+
+        public void LoseHealth(string element)
+        {
+            foreach (var pokemon in Pokemons.ToList())
+            {
+                if (pokemon.Element != element)
+                {
+                    pokemon.Health -= 10;
+                    if (pokemon.Health <= 0)
+                    {
+                        Pokemons.Remove(pokemon);
+                    }
+                }
+            }
+        }
+    }
+}
